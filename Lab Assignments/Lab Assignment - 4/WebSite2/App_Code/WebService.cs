bf0cd1119bdeaa1dabbb34a9d3cd1f9adefdc636
sumanth2109 <sk9cd@mail.umkc.edu>
2014-02-26 22:28:42 -0600

    no uncommitted changes

---
+++ Lab Assignments/Lab Assignment - 4/WebSite2/App_Code/WebService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService {
+
+    public WebService () {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public char AsciiValue(char Character) {
+        return Character;
+    }
+    
+    [WebMethod]
+    public string Course(string Name, int StudentId, string Course, String Faculty) {
+        return "Hey I am " + Name + " (" + StudentId + ")." + " In this semester, I have taken " + Course + " under Dr. " + Faculty + ".";
+    }
+
+    [WebMethod]
+    public int ReverseNumber(int Number)
+    {
+        int result = 0;
+        while (Number > 0)
+        {
+            result = result * 10 + Number % 10;
+            Number /= 10;
+        }
+        return result;
+    }
+
+}
